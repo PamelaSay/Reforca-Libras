4d0b56599e858f32362e0d66d539f8150bab813a
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2026-05-21 20:23:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+//js/script.js
+function abrirjogo(tipo){
+  if(tipo =='potencia1'){
+    alert('Abrindo jogo de Potenciação');
+  }
+  if(tipo === 'radiciacao'){
+    alert('Abrindo jogo de Radiciação');
+  }
+  if(tipo === ' cartesiano'){
+    alert('Abrindo jogo Plano Cartesiano')
+  }
+}
