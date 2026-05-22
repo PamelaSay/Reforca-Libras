1864da657cd76d7afb4e52d94111f25054fb640e
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2026-05-21 23:05:39 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,24 @@
-localStorage.setItem("usuario", nome)
-    function entrar(){
+function entrar(){
 
     let nome = document.getElementById("nome").value
 
+    let senha = document.getElementById("senha").value
+
+    if(nome == "" || senha == ""){
+
+        alert("Preencha todos os campos!")
+
+        return
+    }
+
     localStorage.setItem("usuario", nome)
 
+    alert("Login realizado!")
+
     window.location.href = "index.html"
+}
+
+function voltarPagina(){
+
+    history.back()
 }
