80a2507c946ead4e2da8ae7352b1d4c769fa9294
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2026-05-21 22:14:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //js/script.js
 localStorage.setItem("usuario", nome)
+function entrar(){
 
+    let nome = document.getElementById("nome").value
+
+    localStorage.setItem("usuario", nome)
+
+    window.location.href = "index.html"
+}
 
 
 
