b73e627ee8cd1711a6c5413be044588cd3911be4
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2026-07-29 12:23:11 -0300

    Update jogoradiciacao.js

--- jogoradiciacao.js
+++ jogoradiciacao.js
@@ -1,81 +1,41 @@
 /*radiciacao*/
 let resposta = 25;
-
 let pontos = 0;
-
 let fase = 1;
-
 criarQuadrado(5);
 
 /* CRIAR QUADRADO */
-
 function criarQuadrado(tamanho){
-
-    const quadrado =
-    document.getElementById("quadrado");
-
-    quadrado.innerHTML = "";
-
-    quadrado.style.gridTemplateColumns =
-    `repeat(${tamanho},60px)`;
-
+    const quadrado =document.getElementById("quadrado");quadrado.innerHTML = "";quadrado.style.gridTemplateColumns =`repeat(${tamanho},60px)`;
     for(let i = 0; i < tamanho * tamanho; i++){
-
-        quadrado.innerHTML +=
-        `<div class="quad"></div>`;
-    }
+        quadrado.innerHTML += `<div class="quad"></div>`;}
 }
 
 /* VERIFICAR */
 
 function verificar(valor){
-
     if(valor == resposta){
-
         pontos += 10;
-
         alert("✅ Acertou!");
-
     }else{
-
         alert("❌ Errou!");
     }
 
-    document.getElementById("pontos")
-    .innerHTML = pontos;
+    document.getElementById("pontos").innerHTML = pontos;
 }
 
 /* NOVA PERGUNTA */
-
 function novaPergunta(){
-
     fase++;
-
     if(fase > 10){
-
         alert("🎉 Você terminou o jogo!");
-
         fase = 1;
         pontos = 0;
     }
-
-    document.getElementById("fase")
-    .innerHTML = fase;
-
-    let lado =
-    Math.floor(Math.random() * 6) + 2;
-
+    document.getElementById("fase").innerHTML = fase;
+    let lado = Math.floor(Math.random() * 6) + 2;
     resposta = lado * lado;
-
-    document.getElementById("textoPergunta")
-    .innerHTML =
-    `O quadrado possui lado ${lado}.`;
-
-    criarQuadrado(lado);
-
-    gerarAlternativas();
-
-    atualizarBarra();
+    document.getElementById("textoPergunta").innerHTML =`O quadrado possui lado ${lado}.`;criarQuadrado(lado);gerarAlternativas();atualizarBarra();
 }
 
 /* ALTERNATIVAS */
